70625d1267b627c3331303a112f52b1cd2681ea8
Franco Goti <73183262+FrancoGoti@users.noreply.github.com>
2024-11-21 15:45:46 -0300

    Add files via upload

---
+++ PPAI-2024/PPAI-2024/src/main/java/com/example/PPAI_2024/Ppai2024Application.java
@@ -0,0 +1,13 @@
+package com.example.PPAI_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ppai2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ppai2024Application.class, args);
+	}
+
+}
---
+++ PPAI-2024/PPAI-2024/src/test/java/com/example/PPAI_2024/Ppai2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PPAI_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ppai2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
